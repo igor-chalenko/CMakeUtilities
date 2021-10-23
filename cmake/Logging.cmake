5045f34946059671c756af132de3090cb6c989ed
igor-chalenko <prostakov80@mail.ru>
2021-10-24 00:05:35 +0300

    cmake-utilities-20 Implement nested log categories

--- cmake/Logging.cmake
+++ cmake/Logging.cmake
@@ -15,31 +15,34 @@ include(${CMAKE_CURRENT_LIST_DIR}/GlobalMap.cmake)
 #
 # .. code-block:: cmake
 #
-#    log_message(_level _context _message)
+#    log_message(_level _category _message)
 #
 # Formats and prints the given message. Filters out the messages based on
-# the logging level of the context ``_context``, previously specified by
+# the logging level of the category ``_category``, previously specified by
 # a call to ``log_level``.
 ##############################################################################
-function(log_message _level _context _message)
+function(log_message _level _category _message)
     _log_levels(_levels)
     list(FIND _levels ${_level} _ind)
-    global_get(log.context.${_context}. level _current_level)
-    global_get(log.context.${_context}. file _file_name)
+    global_get(log.category.${_category}. level _current_level)
+    global_get(log.category.${_category}. file _file_name)
 
     if (_current_level STREQUAL "")
-        list(FIND _levels WARN _current_level)
+        _log_parent_level(${_category} _current_level)
+    endif()
+    if (_file_name STREQUAL "")
+        _log_parent_destination(${_category} _file_name)
     endif()
 
     _log_format_message(_formatted_message "${_message}" ${ARGN})
     if (NOT _current_level OR _current_level LESS_EQUAL ${_ind})
         string(TIMESTAMP _timestamp)
         if (_level STREQUAL FATAL)
-            message(FATAL_ERROR "[${_timestamp}][${_context}][${_level}] ${_formatted_message}")
+            message(FATAL_ERROR "[${_timestamp}][${_category}][${_level}] ${_formatted_message}")
         elseif (_level STREQUAL ERROR)
-            message(SEND_ERROR "[${_timestamp}][${_context}][${_level}] ${_formatted_message}")
+            message(SEND_ERROR "[${_timestamp}][${_category}][${_level}] ${_formatted_message}")
         else()
-            set(_message_prefix "[${_timestamp}][${_context}][${_level}]")
+            set(_message_prefix "[${_timestamp}][${_category}][${_level}]")
             if (NOT "${_file_name}" STREQUAL "")
                 file(APPEND "${CMAKE_CURRENT_BINARY_DIR}/${_file_name}" "${_message_prefix} ${_formatted_message}\n")
             else()
@@ -49,26 +52,56 @@ function(log_message _level _context _message)
     endif()
 endfunction()
 
+function(_log_parent_level _category _out_var)
+    if(_category MATCHES "(.+)\\.(.+)")
+        global_get(log.category.${CMAKE_MATCH_1}. level _level)
+        if (NOT _level)
+            _log_parent_level(${CMAKE_MATCH_1}  _new_out_var)
+            set(${_out_var} "${_new_out_var}" PARENT_SCOPE)
+        else()
+            _log_levels(_levels)
+            list(GET _levels ${_level} _el)
+            set(${_out_var} "${_el}" PARENT_SCOPE)
+        endif()
+    else()
+        set(${_out_var} "WARN" PARENT_SCOPE)
+    endif()
+endfunction()
+
+function(_log_parent_destination _category _out_var)
+    if(_category MATCHES "(.+)\\.(.+)")
+        global_get(log.category.${CMAKE_MATCH_1}. file _file)
+        if (NOT _file)
+            _log_parent_destination(${CMAKE_MATCH_1}  _new_out_var)
+            set(${_out_var} "${_new_out_var}" PARENT_SCOPE)
+        else()
+            set(${_out_var} "${_file}" PARENT_SCOPE)
+        endif()
+    else()
+        set(${_out_var} "" PARENT_SCOPE)
+    endif()
+endfunction()
+
 ##############################################################################
 #.rst:
 # .. cmake:command:: log_level
 #
 # .. code-block:: cmake
 #
-#    log_level(_context _level)
+#    log_level(_category _level)
 #
-# All the subsequent messages in the given context ``_context`` will only
-# be printed if their level is at least as high as ``_level``. The levels
-# are defined by the function ``_log_levels`` (the further it is from
-# the beginning, the higher it is). The default level for every context is
-# `INFO`.
+# All the subsequent messages in the given category ``_category`` and its
+# nested categories will only be printed if their level is at least as high
+# as ``_level``. The levels are defined by the function ``_log_levels``
+# (the further it is from the beginning, the higher it is). The default level
+# for every category is `WARN`.
 ##############################################################################
-function(log_level _context _level)
+function(log_level _category _level)
     _log_levels(_levels)
     list(FIND _levels ${_level} _ind)
     if (_ind GREATER -1)
-        global_set(log.context.${_context}. level ${_ind})
-        #message(STATUS "setting log level of ${_context} to ${_ind} (${_level})")
+        global_set(log.category.${_category}. level ${_ind})
+        #message(STATUS "setting log level of ${_category} to ${_ind} (${_level})")
     endif()
 endfunction()
 
@@ -78,13 +111,13 @@ endfunction()
 #
 # .. code-block:: cmake
 #
-#    log_to_file(_context _file_name)
+#    log_to_file(_category _file_name)
 #
-# Directs all subsequent logged messages in the context ``_context`` to a file
-# ``_file_name`` instead of the console.
+# Redirects all subsequent logged messages in the category ``_category`` and
+# its nested categories to a file ``_file_name`` instead of the console.
 ##############################################################################
-function(log_to_file _context _file_name)
-    global_set(log.context.${_context}. file ${_file_name})
+function(log_to_file _category _file_name)
+    global_set(log.category.${_category}. file ${_file_name})
     if (NOT ARGN)
         file(REMOVE ${CMAKE_CURRENT_BINARY_DIR}/${_file_name})
     endif()
@@ -96,15 +129,15 @@ endfunction()
 #
 # .. code-block:: cmake
 #
-#    log_to_console(_context)
+#    log_to_console(_category)
 #
-# Directs all subsequent logged messages in the context ``_context`` to
+# Directs all subsequent logged messages in the category ``_category`` to
 # the console instead of a file, previously specified by a call to
 # ``log_to_file``. Does nothing if the message redirection was not requested
-# for the given context.
+# for the given category.
 ##############################################################################
-function(log_to_console _context)
-    global_unset(log.context.${_context}. file)
+function(log_to_console _category)
+    global_unset(log.category.${_category}. file)
 endfunction()
 
 ##############################################################################
@@ -113,15 +146,15 @@ endfunction()
 #
 # .. code-block:: cmake
 #
-#    log_debug(_context _message <message arguments>)
+#    log_debug(_category _message <message arguments>)
 #
 # Formats the given message and prints it either to a console or to a file.
 # The messages have the following format:
-# [<<timestamp>>][<<context>>][DEBUG] <<message after substitutions>>>
+# [<<timestamp>>][<<category>>][DEBUG] <<message after substitutions>>>
 # This function is a wrapper around ``log_message``.
 ##############################################################################
-function(log_debug _context _message)
-    log_message(DEBUG "${_context}" "${_message}" ${ARGN})
+function(log_debug _category _message)
+    log_message(DEBUG "${_category}" "${_message}" ${ARGN})
 endfunction()
 
 ##############################################################################
@@ -130,15 +163,15 @@ endfunction()
 #
 # .. code-block:: cmake
 #
-#    log_trace(_context _message <message arguments>)
+#    log_trace(_category _message <message arguments>)
 #
 # Formats the given message and prints it either to a console or to a file.
 # The messages have the following format:
-# [<<timestamp>>][<<context>>][TRACE] <<message after substitutions>>>
+# [<<timestamp>>][<<category>>][TRACE] <<message after substitutions>>>
 # This function is a wrapper around ``log_message``.
 ##############################################################################
-function(log_trace _context _message)
-    log_message(TRACE "${_context}" "${_message}" ${ARGN})
+function(log_trace _category _message)
+    log_message(TRACE "${_category}" "${_message}" ${ARGN})
 endfunction()
 
 ##############################################################################
@@ -147,12 +180,12 @@ endfunction()
 #
 # .. code-block:: cmake
 #
-#    log_info(_context _message <message arguments>)
+#    log_info(_category _message <message arguments>)
 #
 # Calls ``log_message`` with the level set to `INFO`.
 ##############################################################################
-function(log_info _context _message)
-    log_message(INFO "${_context}" "${_message}" ${ARGN})
+function(log_info _category _message)
+    log_message(INFO "${_category}" "${_message}" ${ARGN})
 endfunction()
 
 ##############################################################################
@@ -163,12 +196,12 @@ endfunction()
 #
 # .. code-block:: cmake
 #
-#    log_error(_context _message <message arguments>)
+#    log_error(_category _message <message arguments>)
 #
 # Calls ``log_message`` with the level set to `ERROR`.
 ##############################################################################
-function(log_error _context _message)
-    log_message(ERROR "${_context}" "${_message}" ${ARGN})
+function(log_error _category _message)
+    log_message(ERROR "${_category}" "${_message}" ${ARGN})
 endfunction()
 
 ##############################################################################
@@ -177,12 +210,12 @@ endfunction()
 #
 # .. code-block:: cmake
 #
-#    log_warn(_context _message <message arguments>)
+#    log_warn(_category _message <message arguments>)
 #
 # Calls ``log_message`` with the level set to `FATAL`.
 ##############################################################################
-function(log_fatal _context _message)
-    log_message(FATAL "${_context}" "${_message}" ${ARGN})
+function(log_fatal _category _message)
+    log_message(FATAL "${_category}" "${_message}" ${ARGN})
 endfunction()
 
 ##############################################################################
@@ -191,12 +224,12 @@ endfunction()
 #
 # .. code-block:: cmake
 #
-#    log_warn(_context _message <message arguments>)
+#    log_warn(_category _message <message arguments>)
 #
 # Calls ``log_message`` with the level set to `WARN`.
 ##############################################################################
-function(log_warn _context _message)
-    log_message(WARN "${_context}" "${_message}" ${ARGN})
+function(log_warn _category _message)
+    log_message(WARN "${_category}" "${_message}" ${ARGN})
 endfunction()
 
 ##############################################################################
@@ -212,7 +245,7 @@ endfunction()
 # Not a part of the public API.
 ##############################################################################
 function(_log_format_message _out_message _message)
-    global_get(log.context.${_context}. file _file_name)
+    global_get(log.category.${_category}. file _file_name)
 
     set(_index 2)
     foreach(_arg ${ARGN})
--- doc/source/Logging.rst
+++ doc/source/Logging.rst
@@ -2,13 +2,14 @@ Logging
 =======
 
 This module implements a set of functions for logging messages either to
-a console or to a file. Every message has a context that acts as a searchable
-tag, and optional parameters that will be substituted into the message by
-replacing placeholders `{1}`, `{2}`, etc. Output have the following format:
+a console or to a file. Every message has a category that acts as a searchable
+hierarchical tag, and optional parameters that will be substituted into
+the message by replacing placeholders `{1}`, `{2}`, etc. Output have
+the following format:
 
 .. code-block::
 
-  [<timestamp>][<context>][<level>] <message after substitutions>
+  [<timestamp>][<category>][<level>] <message after substitutions>
 
 For example,
 
@@ -20,6 +21,11 @@ For example,
    log_to_file(test test.log)
    # message appended to test.log
    log_warn(test "ICU not found")
+   # enable debug messages
+   log_level(test DEBUG)
+   # printed as well - categories maintain parent-child relation
+   log_info(test.nested "This message should be logged, too.")
+
 
 Notice the parameter `examples` after the main message in the example above.
 Use of parameters is entirely optional; they are only for readability.
@@ -31,7 +37,7 @@ When to use
 Controlled logging is useful in debugging, when it's easy to add a lot of
 calls to `message` and it's hard to remove them afterwards. This is not
 needed with this module - just raise the logging level of the corresponding
-context with either a call to ``log_level`` or via
+category with either a call to ``log_level`` or via
 
 .. code-block::
 
